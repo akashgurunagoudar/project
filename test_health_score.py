729f4f778e9603b4a9d793bd011256d9bc4de9df
akashgurunagoudar <akashgurunagoudar@gmail.com>
2026-01-01 11:35:46 +0530

    Update test_health_score.py

--- test_health_score.py
+++ test_health_score.py
@@ -1,16 +1,24 @@
-from health_score import calculate_health_status
+import unittest
+from health_report import calculate_health_status
 
-def test_excellent():
-    assert calculate_health_status(90) == "Excellent"
 
-def test_good():
-    assert calculate_health_status(75) == "Good"
+class TestHealthStatus(unittest.TestCase):
 
-def test_moderate():
-    assert calculate_health_status(60) == "Moderate"
+    def test_excellent(self):
+        self.assertEqual(calculate_health_status(90), "Excellent")
 
-def test_risk():
-    assert calculate_health_status(45) == "Risk"
+    def test_good(self):
+        self.assertEqual(calculate_health_status(75), "Good")
 
-def test_critical():
-    assert calculate_health_status(30) == "Critical"
+    def test_moderate(self):
+        self.assertEqual(calculate_health_status(60), "Moderate")
+
+    def test_risk(self):
+        self.assertEqual(calculate_health_status(45), "Risk")
+
+    def test_critical(self):
+        self.assertEqual(calculate_health_status(30), "Critical")
+
+
+if __name__ == "__main__":
+    unittest.main()
